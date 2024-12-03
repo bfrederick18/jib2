cbc16fe35134733a05a4e73501633edc1b961137
inigojensen <inigojensen@gmail.com>
2024-12-03 14:33:37 -0800

    Non-working squat

--- jib2/squat.py
+++ jib2/squat.py
@@ -1,10 +1,5 @@
 '''squat.py
 
-   This is a demo for moving/placing an ungrounded robot and moving joints.
-
-   In particular, imagine a humanoid robot.  This moves/rotates the
-   pelvis frame relative to the world.  And waves an arm.
-
    Node:      /squat
    Publish:   /joint_states             sensor_msgs.msg.JointState
    Broadcast: 'pelvis' w.r.t. 'world'   geometry_msgs.msg.TransformStamped
@@ -14,7 +9,7 @@
 import rclpy
 import numpy as np
 
-from math import pi, sin, cos, acos, atan2, sqrt, fmod, exp
+from math import cos
 
 from rclpy.node                 import Node
 from rclpy.time                 import Duration
@@ -23,6 +18,7 @@ from geometry_msgs.msg          import TransformStamped
 from sensor_msgs.msg            import JointState
 
 from hw5code.TransformHelpers     import *
+from hw6code.KinematicChain       import KinematicChain
 
 
 #
@@ -78,7 +74,24 @@ class DemoNode(Node):
         self.z_pelvis_mid = (self.Z_PELVIS_TOP + self.Z_PELVIS_LOW) / 2
         self.z_pelvis_A = (self.Z_PELVIS_TOP - self.Z_PELVIS_LOW) / 2  # amplitude
 
-        # Inverse kinamatic variables
+        self.chain_left_leg = KinematicChain(self, 'pelvis', 'l_foot', jointnames[0:6])  # nope
+        self.chain_test_0001 = KinematicChain(self, 'pelvis', 'l_foot', [jointnames[i] for i in [2, 0, 1, 3, 5, 4]])  # nope
+
+        self.chain_right_leg = KinematicChain(self, 'pelvis', 'r_foot', jointnames[6:12])    # untested
+        self.chain_torso = KinematicChain(self, 'pelvis', 'u_torso', jointnames[12:16])      # untested
+        self.chain_left_arm = KinematicChain(self, 'u_torso', 'l_hand', jointnames[16:23])   # untested
+        self.chain_right_arm = KinematicChain(self, 'u_torso', 'r_hand', jointnames[23:30])  # untested
+        
+        self.q = np.zeros(len(jointnames))
+        self.qdot = np.zeros(len(jointnames))
+
+        (ptip0, Rtip0, Jv0, Jw0) = self.chain_left_leg.fkin(self.q[0:6])
+
+        self.pd = ptip0
+
+        self.K_p = 50.0
+        self.gamma = 0.1
+        self.K_s = 1.0
 
 
     def shutdown(self):
@@ -104,35 +117,31 @@ class DemoNode(Node):
         trans.transform       = Transform_from_T(T_pelvis)
         self.broadcaster.sendTransform(trans)
 
-        q    = np.zeros(len(jointnames))
-        qdot = np.zeros(len(jointnames))
+        (ptip, Rtip, Jv, Jw) = self.chain_left_leg.fkin(self.q[0:6])
 
-        # not moving in the x or z; just toe over the knee
-        I_LHIP_Y = jointnames.index('l_leg_hpy')
-        I_RHIP_Y = jointnames.index('r_leg_hpy')
-        I_LKNEE_Y = jointnames.index('l_leg_kny')
-        I_RKNEE_Y = jointnames.index('r_leg_kny')
-        I_LANKLE_Y = jointnames.index('l_leg_aky')
-        I_RANKLE_Y = jointnames.index('r_leg_aky')
+        err = self.pd - ptip
 
+        gamma = self.gamma
+        weight_mat = gamma**2 * np.eye(6)
+        Jwinv = np.linalg.pinv(Jv.T @ Jv + weight_mat) @ Jv.T
 
+        qdot_primary = Jwinv @ (self.K_p * err)
 
-        
-        """ OLD SHIT WE DUNNO MAYBE THE WAVE
-        i_rknee_y = jointnames.index('r_leg_kny')
-        i_lknee_y = jointnames.index('l_leg_kny')
+        q0 = np.zeros(6)  # desired nominal joint positions
+        qsdot = -self.K_s * (self.q[0:6] - q0)
 
-        i_relbow = jointnames.index('r_arm_elx')
+        I = np.eye(6)
+        qdot_sec = (I - Jwinv @ Jv) @ qsdot
 
-        q[i_relbow]    = - pi/2 + pi/8 * sin(2*self.t)
-        qdot[i_relbow] =          pi/4 * cos(2*self.t)
-        """
+        qdot_left_leg = qdot_primary + qdot_sec
+
+        self.q[0:6] += qdot_left_leg * self.dt
 
         cmdmsg = JointState()
         cmdmsg.header.stamp = self.now().to_msg()       # Current time for ROS
         cmdmsg.name         = jointnames                # List of names
-        cmdmsg.position     = q.flatten().tolist()      # List of positions
-        cmdmsg.velocity     = qdot.flatten().tolist()   # List of velocities
+        cmdmsg.position     = self.q.tolist()           # List of positions
+        cmdmsg.velocity     = self.qdot.tolist()        # List of velocities
         self.pub.publish(cmdmsg)
 
 
